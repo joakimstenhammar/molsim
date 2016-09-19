212e58d9a4e24be3daa666f32775dce0c520384e
Pascal Hebbeker <pascal.hebbeker@rwth-aachen.de>
2016-09-19 16:31:05 +0200

    Added test file

--- Testin/goall.sh
+++ Testin/goall.sh
@@ -277,6 +277,7 @@ $molsim  ltime                    $core
 #complexation-analysis
 $molsim  hierarchical.mc          $core
 $molsim  copoly-seq.mc            $core
+$molsim  complexation.mc          $core
 
 echo
 echo "Remove group and dump files"
