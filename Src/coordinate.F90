a405d9851527d4f42eb7974acef9ebd3fcb42000
Cornelius Hofzumahaus <cornelius.hofzumahaus@rwth-aachen.de>
2016-10-24 10:40:39 +0200

    Migration of nnwt, nnwnwt completed

--- Src/coordinate.F90
+++ Src/coordinate.F90
@@ -47,13 +47,8 @@ module CoordinateModule
    integer(4)    :: iptnode                   ! particle type of nodes (for SetPeriodicNetwork)
    integer(4)    :: ictstrand                 ! chain type of strands
 
-   integer(4), parameter :: mnnetworkt  = 2   ! maximum number of network types (for SetNetwork)
-   integer(4)    :: nnetworkt                 ! number of network types
-   integer(4)    :: nnetwork(mnnetworkt)      ! number of networks of network type inetworkt
-   real(8)       :: rnetwork(mnnetworkt)      ! radius of network type inetworkt
-   integer(4)    :: iptnodenwt(mnnetworkt)    ! particle type of nodes of different network types
-   integer(4)    :: ictstrandnwt(mnnetworkt)  ! chain type of strands of different network types
-   character(8)  :: txorigin(mnnetworkt)      ! network center of net type inetworkt ("origin" and "random")
+   real(8)       :: rnwt(mnnwt)               ! radius of network type inwt
+   character(8)  :: txorigin(mnnwt)           ! network center of network type inwt ("origin" and "random")
 
    real(8)       :: radlimit(2)               ! lower and upper radial limit of particles (for SetCoreCell)
 
