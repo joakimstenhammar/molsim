b1fd97f7ac484ca0ebe14ba32ae4d7616d457cb7
Pascal Hebbeker <pascal.hebbeker@rwth-aachen.de>
2017-06-22 15:21:10 +0200

    have molsim_save update timestamp of save files when they are already correct, clean up special cases

--- Testin/scripts/molsim_save.sh
+++ Testin/scripts/molsim_save.sh
@@ -2,8 +2,9 @@
 
 stable="stable.md5sum"
 current=".current.md5sum"
-dir=$(dirname $1)
-file=$(basename $1)
+in=$1
+dir=$(dirname $in)
+file=$(basename $in)
 pro=${file%.*}
 
 molsim="../../Bin/molsim_ser.exe"
@@ -11,15 +12,15 @@ if [ "$dir" == "save" ]; then
    if [ -f $dir/$pro.version ]; then
       if `cmp --silent $stable $dir/$pro.version`; then
          echo "This file was created with the currently stable version. Not running molsim again."
+         touch $dir/$pro.*
          exit 0
       fi
    fi
    if ! `cmp --silent $stable $current`; then
-      echo "ERROR: $1 must generated with a stable version."; exit 1
+      echo "ERROR: $in must generated with a stable version."; exit 1
    fi
 fi
 cd $dir && pwd && $molsim $pro; e=$? && cd -
 if [ "$dir" == "save" ]; then
    cat $current > $dir/$pro.version
 fi
-exit $e
