f6242bee7dbf446385f4a7e175162d8bfe67e8ae
Pascal Hebbeker <pascal.hebbeker@rwth-aachen.de>
2017-07-10 16:14:26 +0200

    fix parallel bcast in dump

--- Src/dump.F90
+++ Src/dump.F90
@@ -216,6 +216,7 @@ subroutine DoDump(str)
 
    integer(4)   :: ip, ia, m, idum = 0
    real(8)      :: dum
+   real(8)      :: rdutot
    logical      :: ldum
 
    if (str(1:4) == 'open' .and. master) then
@@ -280,7 +281,9 @@ subroutine DoDump(str)
       if (ldidm) call par_bc_reals(idmo   , 3*(ipupp-iplow+1))
       if (ldlaz) call par_bc_reals(az     ,   (ipupp-iplow+1))
       if (ldlaz) call par_bc_logicals(laz ,   (ipupp-iplow+1))
-      if (ldutot) call par_bc_reals(u%tot*sclene/(np*GasConstant*temp*scltem), 1)
+      rdutot = u%tot*sclene/(np*GasConstant*temp*scltem)
+      if (ldutot) call par_bc_real(rdutot)
+      u%tot = u%tot/(sclene/(np*GasConstant*temp*scltem))
 #endif
 
       call QuaToOri(np, iplow, ipupp, qua, ori)
