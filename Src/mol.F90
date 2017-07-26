3790f50e3c48fcbc8160bba8c2e48eaf77d6ec00
Pascal Hebbeker <pascal.hebbeker@rwth-aachen.de>
2017-07-26 16:29:06 +0200

    increment version

--- Src/mol.F90
+++ Src/mol.F90
@@ -193,7 +193,7 @@ module MolModule
 
 ! ... version, date and author
 
-   character(29) :: txVersionDate = 'version 6.4.7, v4.2.0'
+   character(29) :: txVersionDate = 'version 6.4.7, v4.2.1'
    character(9)  :: txAuthor      = 'Per Linse'
 
 ! ... external units
