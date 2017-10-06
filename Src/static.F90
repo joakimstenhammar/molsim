39bc5b8c367fd3ec50b900b666a53d37cc5569a8
Pascal Hebbeker <pascal.hebbeker@rwth-aachen.de>
2017-10-06 19:57:55 +0200

    set correct nvar in static.F90

--- Src/static.F90
+++ Src/static.F90
@@ -4215,6 +4215,8 @@ subroutine ChainDF(iStage)
             end do
          end if
       end do
+      ! set nvar accoding to ivar, to skipped chain if hierachical structure
+      nvar = ivar
       call DistFuncSample(iStage, nvar, var)
 
    case (iBeforeSimulation)
