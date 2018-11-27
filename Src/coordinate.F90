b87a290f33d3152141624c0ffa3463df77e4e32a
Cornelius Hofzumahaus <cornelius.hofzumahaus@rwth-aachen.de>
2018-11-27 11:37:02 +0100

    Fix issue 10: Prevent stack overflow due to recursive UndoPBC and prevent integer overflow of jpnlist (#21)
    * Rename subroutine UndoPBC and UndoPBCModule
    
    * Add new subroutine UndoPBC which uses an iterative strategy
    
    * Change kind of jpnlist in mol.F90 from integer(2) to integer(4) to prevent integer overflow
    
    * Fix if-statement and include master from MolModule
    
    * Process bond information in module UndoPBCModule
    
    * Finalized subroutine UndoPBC
    
    * Incremented version on patch level to 4.6.1
    
    * Removed old subroutine UndoPBC (recursive)
    
    * check hierarchical and network in SetRandomChain
    
    Before `lnetwork` was not checked if a hierarchical structure was
    present in the system. But a particle set by SetChainRandom should be
    neither in a hierarchical structure nor in a network. Therefore we need
    both checks.
    
    * test input for undopbc and vtfimage
    
    * Finalize pull request: Update CHANGELOG, declare stable and recompile documentation

--- Src/coordinate.F90
+++ Src/coordinate.F90
@@ -1633,7 +1633,8 @@ subroutine SetChainRandom !(iptset) iptset is not needed
          if ((ipt == iptnode) .or. (ict == ictstrand)) cycle ! exclude chains which are nodes or strands
          if (lhierarchical) then
             if(ihnpn(ip) /= 0) cycle                        ! exclude chains which are in hierarchical structure
-         else if (lnetwork) then
+         end if
+         if (lnetwork) then
             if (sum(ncctnwt(ict,1:nnwt)) > 0) cycle         ! exclude chains which are in finite network structure
          end if
       ! MORE
@@ -2321,7 +2322,6 @@ subroutine SetNetwork(ipt)
    end if
 
 ! ... set gels
-
    maxnbondcl(ipt) = nclnode
    inwglob = sum(nnwnwt(1:inwt-1))
 
